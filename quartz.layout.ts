9e8b281fd5c8ab8430977cfe8a1e397fecf092ed
LegacyxAI <zeus.ops@legacyhq.ai>
2026-03-23 02:58:53 -0400

    Revert explorer to default collapsed state

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -54,7 +54,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer({ folderDefaultState: "open" }),
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
@@ -81,7 +81,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer({ folderDefaultState: "open" }),
+    Component.Explorer(),
   ],
   right: [],
 }
