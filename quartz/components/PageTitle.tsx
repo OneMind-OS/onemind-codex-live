8e8b93665278a98dd3edfa2769588a4eb7bcf74f
LegacyxAI <zeus.ops@legacyhq.ai>
2026-03-23 03:04:54 -0400

    Triple logo emblem size to 156px

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -11,8 +11,8 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   return (
     <h2 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>
-        <img src={logoLightPath} alt="OneMind" class="page-logo page-logo-light" width="52" height="52" />
-        <img src={logoDarkPath} alt="OneMind" class="page-logo page-logo-dark" width="52" height="52" />
+        <img src={logoLightPath} alt="OneMind" class="page-logo page-logo-light" width="156" height="156" />
+        <img src={logoDarkPath} alt="OneMind" class="page-logo page-logo-dark" width="156" height="156" />
         <span>{title}</span>
       </a>
     </h2>
@@ -33,7 +33,7 @@ PageTitle.css = `
 }
 
 .page-logo {
-  height: 52px;
+  height: 156px;
   width: auto;
   flex-shrink: 0;
   object-fit: contain;
